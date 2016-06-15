0602c324b1167ac0595980284ee6f406d020eedf
Danny Tunon <danieltunon@gmail.com>
2016-06-15 08:20:00 -0700

    Implement /isPalindromePermutation/

---
+++ Arrays and Strings (Ch 1)/palindromePermutation.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const charCounter = require('./checkPermutation').charCounter;
+// Given a string check if it is a permutation of a palindrome.
+// Does not need to be limited to dictionary words.
+
+function isPalindromePermutation(string) {
+  const charCount = charCounter(string);
+  delete charCount[' '];
+  return Object.values(charCount).filter(n => n % 2 !== 0).length <= 1;
+}
+
+assert(
+  isPalindromePermutation('coat act') === true,
+  'should return true if string can be rearranged into a palindrome'
+);
+assert(
+  isPalindromePermutation('happy fact') === false,
+  'should return false if string can\'t be rearranged into a palindrome'
+);
