bed3b01303567aa1676089311cd7a97ea3c3bf37
Danny Tunon <danieltunon@gmail.com>
2016-06-18 11:15:52 -0700

    Initial implementation doesn't pass all tests

--- Arrays and Strings (Ch 1)/1.5-oneAway.js
+++ Arrays and Strings (Ch 1)/1.5-oneAway.js
@@ -1,15 +1,37 @@
 const assert = require('assert');
-
+const charCounter = require('./1.2-checkPermutation').charCounter;
 // There are theree kinds of edits that can be performed on a string:
 // insert a character, remove a character, replace a character.
 // Write a func that takes 2 strings and returns ture if they are only
 // one or zero edits away.
 
 function oneAway(str1, str2) {
+  let smallerCount;
+  let largerCount;
 
+  if (str1.length > str2.length) {
+    largerCount = charCounter(str1);
+    smallerCount = charCounter(str2);
+  } else {
+    largerCount = charCounter(str2);
+    smallerCount = charCounter(str1);
+  }
+
+  if (Object.keys(largerCount) - Object.keys(smallerCount) > 1) {
+    return false;
+  }
+
+  const numOfChanges = Object.keys(largerCount).reduce((changeCount, key) => {
+    if (smallerCount[key] === undefined || largerCount[key] - smallerCount[key] !== 0) {
+      return changeCount + 1;
+    }
+    return changeCount;
+  }, 0);
+
+  return numOfChanges <= 1;
 }
 
-/******** Notes ********
+/******** Notes ********/
 
 
 /******** Tests ********/
