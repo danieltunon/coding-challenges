4b9e5391f68458e79067584bce5d23109a1e6d66
Danny Tunon <danieltunon@gmail.com>
2016-07-01 15:44:33 -0700

    Refactor assert to use toString method from LL

--- Linked Lists (Ch 2)/1.1-removeDups.js
+++ Linked Lists (Ch 2)/1.1-removeDups.js
@@ -30,5 +30,5 @@ function removeDupsSet(ll) {
 
 const ll1 = Object.create(SinglyLinkedList).init(1, 2, 3, 1, 2, 5, 2, 3, 6);
 const ll2 = Object.create(SinglyLinkedList).init(1, 2, 3, 5, 6);
-assert.deepEqual(removeDupsSet(ll1), ll2, 'Should remove duplicates from linked list with Set implementation');
-assert.deepEqual(ll1.removeDups(), ll2, 'Should remove duplicates from linked list without a buffer');
+assert(removeDupsSet(ll1).toString(), ll2.toString(), 'Should remove duplicates from linked list with Set implementation');
+assert(ll1.removeDups().toString(), ll2.toString(), 'Should remove duplicates from linked list without a buffer');
