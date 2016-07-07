967eb9eae697fdf1294034dec41a261238a38278
Danny Tunon <danieltunon@gmail.com>
2016-07-07 15:37:15 -0700

    Create /isPartitioned/ helper to do tests

--- Linked Lists (Ch 2)/2.4-partition.js
+++ Linked Lists (Ch 2)/2.4-partition.js
@@ -8,4 +8,19 @@ import { SinglyLinkedList } from '../DataStructures';
 // I: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
 // O: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 
-// assert()
+function isPartitioned(partition, ll) {
+  let inRightPartition = false;
+  let areAllGTE = true;
+  for (const node of ll) {
+    if (inRightPartition) {
+      areAllGTE = areAllGTE && node.val >= partition;
+    } else {
+      if (node.val >= partition) inRightPartition = true;
+    }
+  }
+  return areAllGTE;
+}
+const sll1 = Object.create(SinglyLinkedList).init(3,5,8,5,10,2,1);
+const partitionedSll1 = Object.create(SinglyLinkedList).init(3,1,2,10,5,5,8);
+assert(isPartitioned(5, partitionedSll1), 'should return true if the LL is partitioned');
+assert(isPartitioned(5, sll1) === false, 'should return false if the LL is not partitioned');
