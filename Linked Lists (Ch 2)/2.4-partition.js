f2650b51fc6ac31c1e8c88d7be344c1fcb51683e
Danny Tunon <danieltunon@gmail.com>
2016-07-07 15:43:27 -0700

    Implement partition functionality using addToTail

--- Linked Lists (Ch 2)/2.4-partition.js
+++ Linked Lists (Ch 2)/2.4-partition.js
@@ -8,6 +8,19 @@ import { SinglyLinkedList } from '../DataStructures';
 // I: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
 // O: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 
+// Trivial with addToTail method
+function partition(partitionValue, sll) {
+  const partitonedList = Object.create(SinglyLinkedList);
+  for (const val of sll.values()) {
+    if (val < partitionValue) {
+      partitonedList.addToHead(val);
+    } else {
+      partitonedList.addToTail(val);
+    }
+  }
+  return partitonedList;
+}
+
 function isPartitioned(partition, ll) {
   let inRightPartition = false;
   let areAllGTE = true;
@@ -24,3 +37,4 @@ const sll1 = Object.create(SinglyLinkedList).init(3,5,8,5,10,2,1);
 const partitionedSll1 = Object.create(SinglyLinkedList).init(3,1,2,10,5,5,8);
 assert(isPartitioned(5, partitionedSll1), 'should return true if the LL is partitioned');
 assert(isPartitioned(5, sll1) === false, 'should return false if the LL is not partitioned');
+assert(isPartitioned(5, partition(5, sll1)), 'should partition the LL around the given value');
