52a12cc4b27b1f4becef1bec885f13da6afb17c0
Danny Tunon <danieltunon@gmail.com>
2016-07-07 10:36:59 -0700

    Add prompt and example

---
+++ Linked Lists (Ch 2)/2.4-partition.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import { SinglyLinkedList } from '../DataStructures';
+
+// Write a function that partitions a linked list around a value x, such that
+// all nodes less than x come before all nodes greater than or equal to x.
+// The partition element x can appear anywhere in the 'right' partition.
+// Eg.
+// I: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+// O: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+// assert()
