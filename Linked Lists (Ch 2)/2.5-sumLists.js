2bc13cb049aa0e770aaa9f2e1cbc8ff0289a23db
Danny Tunon <danieltunon@gmail.com>
2016-07-16 11:11:13 -0700

    Implement /sumListsForward/ but not handling all test cases

--- Linked Lists (Ch 2)/2.5-sumLists.js
+++ Linked Lists (Ch 2)/2.5-sumLists.js
@@ -11,6 +11,7 @@ import { SinglyLinkedList } from '../DataStructures';
 //imperativey solution
 function sumLists(sll1, sll2) {
   const sum = Object.create(SinglyLinkedList).init();
+
   let node1 = sll1.head;
   let node2 = sll2.head;
   let remainder = 0;
@@ -21,6 +22,35 @@ function sumLists(sll1, sll2) {
     remainder = ~~(remainder / 10);
     node1 = node1 && node1.next;
     node2 = node2 && node2.next;
+  }
+
+  return sum;
+}
+
+// follow up:
+// Suppose the digits are stored in forward order.
+// Example:
+// I: (6 -> 1 -> 7) + (2 -> 9 -> 5) i.e. 617 + 295
+// O: (9 -> 1 -> 2)  i.e. 912
+function sumListsForward(sll1, sll2) {
+  const sum = Object.create(SinglyLinkedList).init();
+
+  const difference = sll1.size - sll2.size;
+  for (let i = 0; i < Math.abs(difference); i++) {
+    difference < 0 ? sll1.addToHead(0) : sll2.addToHead(0);
+  }
+
+  let node1 = sll1.head;
+  let node2 = sll2.head;
+  let remainder = 0;
+  remainder += (sll1.head && sll1.head.val) + (sll2.head && sll2.head.val);
+  while (node1 || node2 || remainder) {
+    node1 = node1 && node1.next;
+    node2 = node2 && node2.next;
+    remainder *= 10;
+    remainder += (node1 && node1.val) + (node2 && node2.val);
+    sum.addToTail(~~(remainder / 10));
+    remainder %= 10;
   }
 
   return sum;
@@ -38,3 +68,16 @@ const n6439 = Object.create(SinglyLinkedList).init(9, 3, 4, 6);
 const n7825 = Object.create(SinglyLinkedList).init(5, 2, 8, 7);
 const n14264 = Object.create(SinglyLinkedList).init(4, 6, 2, 4, 1);
 assert.deepEqual([...sumLists(n6439, n7825).values()], [...n14264.values()], 'should return the correct sum');
+const N617 = Object.create(SinglyLinkedList).init(6, 1, 7);
+const N295 = Object.create(SinglyLinkedList).init(2, 9, 5);
+const N912 = Object.create(SinglyLinkedList).init(9, 1, 2);
+assert.deepEqual([...sumListsForward(N617, N295).values()], [...N912.values()], 'should return the correct sum');
+const N1417 = Object.create(SinglyLinkedList).init(1, 4, 1, 7);
+const N95 = Object.create(SinglyLinkedList).init(9, 5);
+const N1512 = Object.create(SinglyLinkedList).init(1, 5, 1, 2);
+assert.deepEqual([...sumListsForward(N1417, N95).values()], [...N1512.values()], 'should return the correct sum');
+const N6439 = Object.create(SinglyLinkedList).init(6, 4, 3, 9);
+const N7825 = Object.create(SinglyLinkedList).init(7, 8, 2, 5);
+const N14264 = Object.create(SinglyLinkedList).init(1, 4, 2, 6, 4);
+console.log([...sumListsForward(N6439, N7825).values()])
+assert.deepEqual([...sumListsForward(N6439, N7825).values()], [...N14264.values()], 'should return the correct sum');
