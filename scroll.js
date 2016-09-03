e7d569370f8516bab5f5d52ecea87ea7a00ce0a3
Danny Tunon <danieltunon@gmail.com>
2016-09-02 18:26:41 -0700

    Implement infinite scrolling by appending divs

--- scroll.js
+++ scroll.js
@@ -1,1 +1,33 @@
-document.addEventListener('DOMContentLoaded', (e) => console.log('heard an onload event'));
+/* global R */
+(function() {
+  document.addEventListener('DOMContentLoaded', (e) => addNCards(20));
+
+  window.addEventListener('scroll', () => {
+    if (((window.innerHeight + window.scrollY) / document.body.scrollHeight) > 0.9) {
+      addNCards(20);
+    }
+  });
+
+  function createCard() {
+    const card = document.createElement('div');
+    card.className = 'card';
+    return card;
+  }
+
+  const addBgColor = function() {
+    const colors = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed ", "Indigo  ", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen "];
+    let i = 0;
+
+    return function(card) {
+      card.style['background-color'] = colors[i++ % colors.length];
+      return card;
+    };
+  }();
+
+  function addNCards(n) {
+    if (n == 0) return;
+
+    document.querySelector('main').appendChild(addBgColor(createCard()));
+    addNCards(n - 1);
+  }
+})();
