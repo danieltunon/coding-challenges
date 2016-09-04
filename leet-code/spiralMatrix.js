d7c606be0a908b291d65e9a4c2c10f8d19a5b7df
Danny Tunon <danieltunon@gmail.com>
2016-09-03 22:30:45 -0700

    Going all while loopy

--- leet-code/spiralMatrix.js
+++ leet-code/spiralMatrix.js
@@ -1,14 +1,41 @@
 /**
- * @param {number[][]} matrix
- * @return {number[]}
- */
+* @param {number[][]} matrix
+* @return {number[]}
+*/
 var spiralOrder = function(matrix) {
-  function traverseRow(matrix, row, col) {
-    if (col < matrix[row].length) {
-      return [matrix[row][col]].concat(traverse(matrix, col + 1, row));
+  let upperBound = 0;
+  let leftBound = 0;
+  let lowerBound = matrix.length - 1;
+  let rightBound = matrix[0].length - 1;
+  let row = 0;
+  let col = 0;
+  const spiral = [];
+  debugger;
+  while (leftBound != rightBound && upperBound != lowerBound) {
+    while (col <= rightBound) {
+      spiral.push(matrix[upperBound][col++]);
     }
-    if (row < matrix.length) {
-      return [matrix[row][col - 1]].concat(traverse(matrix, col - 1, row + 1));
+    row = ++upperBound;
+    while (row <= lowerBound) {
+      spiral.push(matrix[row++][rightBound]);
     }
+    col  = --rightBound;
+    while (col >= leftBound) {
+      spiral.push(matrix[lowerBound][col--]);
+    }
+    row = --lowerBound;
+    while (row >= upperBound) {
+      spiral.push(matrix[row--][leftBound]);
+    }
+    col = ++leftBound;
   }
+  return spiral;
 };
+
+const matrix = [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+];
+
+console.log(spiralOrder(matrix))
