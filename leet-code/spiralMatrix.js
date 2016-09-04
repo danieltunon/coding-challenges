34536c5983f2b5f2f1358a4616d6332f51f795b9
Danny Tunon <danieltunon@gmail.com>
2016-09-04 00:41:30 -0700

    Ramda no play nice

--- leet-code/spiralMatrix.js
+++ leet-code/spiralMatrix.js
@@ -1,42 +1,38 @@
 const assert = require('assert');
-const { head, tail, last, init, map } = require('ramda');
+const { head, tail, last, init, map, reverse } = require('ramda');
 /**
 * @param {number[][]} matrix
 * @return {number[]}
 */
 var spiralOrder = function(matrix) {
-  // console.log(matrix)
+  debugger;
   if (matrix.length == 0) return [];
   return [
     ...head(matrix),
+    ...lastCol(tail(matrix)),
+    ...reverse(init(initCol(tail(matrix)))),
+    ...firstCol(init(initCol(tail(matrix)))),
+    spiralOrder(restCol(init(initCol(tail(matrix))))),
   ]
 };
 
 function firstCol(matrix) {
   if (matrix.length == 0 || matrix[0].length == 1) return [];
   return matrix.map(head).reverse();
-  // return matrix.map(row => row[0]);
 }
 
 function restCol(matrix) {
-  // return matrix.map(row => row.filter((col, i) => i != 0));
   if (matrix.length == 0 || matrix[0].length == 1) return [];
-  // return matrix.reduce((cols, row) => row.length > 1 ? cols.concat([row.slice(1)]) : cols, []);
   return matrix.map(tail);
 }
 
 function lastCol(matrix) {
   if (matrix.length == 0 || matrix[0].length == 1) return [];
-
-  // return matrix.reduce((col, row) => col.concat(row[row.length - 1]));
-  // return matrix.map(row => row[row.length - 1]);
   return matrix.map(last);
 }
 
 function initCol(matrix) {
   if (matrix.length == 0 || matrix[0].length == 1) return [];
-  // return matrix.map(row => row.slice(0, row.length - 1));
-  // return matrix.reduce((cols, row) => row.length > 1 ? cols.concat([row.slice(0, row.length - 1)]) : cols, []);
   return matrix.map(init);
 }
 
@@ -48,10 +44,18 @@ const matrix = [
 const oneRow = [[1, 2, 3]];
 const oneCol = [[1], [2], [3]];
 
+assert.deepEqual(head(matrix), [1,2,3])
+assert.deepEqual(head([[5]]), [5])
+assert.deepEqual(tail(matrix), [[ 4, 5, 6 ], [ 7, 8, 9 ]])
+assert.deepEqual(tail(oneRow), [])
+assert.deepEqual(reverse(last(matrix)), [ 9, 8, 7 ])
+assert.deepEqual(init(matrix), [[ 1, 2, 3 ], [ 4, 5, 6 ]])
+assert.deepEqual(init(oneRow), [])
 assert.deepEqual(firstCol(matrix), [7, 4, 1])
 assert.deepEqual(restCol(matrix), [[2, 3], [5, 6], [8, 9]])
 assert.deepEqual(restCol(oneCol), [])
 assert.deepEqual(lastCol(matrix), [3, 6 ,9])
 assert.deepEqual(initCol(oneCol), [])
-assert.deepEqual(spiralOrder(matrix), [1,2,3,6,9,8,7,4,5])
+// assert.deepEqual(spiralOrder(matrix), [1,2,3,6,9,8,7,4,5])
+assert.deepEqual(spiralOrder([[5]]), [5])
 // console.log(step(firstCol, restCol, step(lastRow, initRow, step(lastCol, initCol, step(firstRow, restRow, {matrix: matrix, result: []})))));
